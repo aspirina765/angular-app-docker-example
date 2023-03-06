d1b7929a49fec044da581a671f4cf7bfc67c8770
aspirina765 <molinalucas765@gmail.com>
2023-03-06 18:50:45 -0300

    docs: Add docs and refeact

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 ## Test Suite para Angular com NodeJS 16.19.0
 
-Este repositório clona um repositório de 
+Este repositório clona um repositório de _**Angular**_ e _**NodeJS versão 16.19.0**_ por meio do bash script _**run-git-repo.sh**_ e o testa em _**docker**_ e/ou _**docker-compose**_.
+
+### Configuração Rápida
+
+Basta configurar a _**url**_, a _**branch**_ e a _**tag**_ (**não obrigatória**, basta deixar "" vazia) do repositório de github que se quer clonar (na pasta ```respo-test/```)
+
+``` console
+GITHUB_REPO_URL="" 
+BRANCH="" 
+TAG=""
+```
+![img.png](docs/img.png)
+
+no arquivo ```run-git-repo.sh``` , e executá-lo pelo comando: 
+
+``` bash
+bash run-git-repo.sh
+```
+
+E então o teste será executado. 
+
+Existem duas opções para teste, em ```docker``` e em ```docker-compose``` (no mesmo arquivo ```run-git-repo.sh```), conforme imagem abaixo:
+
+![img_2.png](docs/img_2.png)
+
+Para usar uma e/ou outra, basta _**comentar**_ ou _**descomentar**_ os comandos correspondentes.
 
--- run-git-repo.sh
+++ run-git-repo.sh
@@ -2,17 +2,20 @@
 
 set -e
 
+GITHUB_REPO_URL="https://github.com/aspirina765/angular-app-docker-example-1.git" ;
+BRANCH="master" ;
+TAG="" ;
+
 CURRENT_PATH="$(pwd)" ;
 cd "${CURRENT_PATH}" ;
 
-GITHUB_REPO_URL="https://github.com/aspirina765/angular-app-docker-example-1.git" ;
-BRANCH="master" ;
 REPO_NAME=$(eval 'basename -s .git $GITHUB_REPO_URL') ;
 PATH_REPO_TEST="${CURRENT_PATH}/repo-test" ;
 
-#mkdir -p "${CURRENT_PATH}/repo-test" ;
 sudo chown -R $USER:users "${CURRENT_PATH}/repo-test" ;
 rm -rf "${PATH_REPO_TEST}/${REPO_NAME}" ;
+
+# git checkout $tag -b latest
 
 sudo chown -R $USER:users $PATH_REPO_TEST ;
 
@@ -21,7 +24,14 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else
     echo "$FILE does not exist."
-    git clone --branch $BRANCH $GITHUB_REPO_URL "${PATH_REPO_TEST}/${REPO_NAME}" ;
+    if [ -z "$TAG"  ]; then
+      echo "Repo tag ${TAG} DON'T exist"
+      git clone --branch $BRANCH $GITHUB_REPO_URL "${PATH_REPO_TEST}/${REPO_NAME}" ;
+    else
+      echo "Repo tag ${TAG} exists"
+      git clone --branch $BRANCH $GITHUB_REPO_URL "${PATH_REPO_TEST}/${REPO_NAME}" ;
+      cd "${PATH_REPO_TEST}/${REPO_NAME}" && git checkout $TAG
+    fi
 fi
 
 {
@@ -41,13 +51,13 @@ cd "${PATH_REPO_TEST}/${REPO_NAME}" && PATH_TO_PACKAGE_JSON=$(eval 'find . -type
 
 echo $PATH_TO_ANGULAR_REPO ;
 
-cp "${CURRENT_PATH}/nodejs.Dockerfile" "${PATH_TO_ANGULAR_REPO}/Dockerfile"  ;
-cp "${CURRENT_PATH}/nodejs.compose.yaml" "${PATH_REPO_TEST}/${REPO_NAME}/compose.yaml"  ;
+cp "${CURRENT_PATH}/nodejs.Dockerfile" "${PATH_TO_ANGULAR_REPO}/nodejs.Dockerfile"  ;
+cp "${CURRENT_PATH}/nodejs.compose.yaml" "${PATH_REPO_TEST}/${REPO_NAME}/nodejs.compose.yaml"  ;
 
 ## OPÇÃO 1:
-cd "${PATH_TO_ANGULAR_REPO}" && docker build -t my-angular-app . ;
+cd "${PATH_TO_ANGULAR_REPO}" && docker build -t my-angular-app -f "${PATH_TO_ANGULAR_REPO}/nodejs.Dockerfile" . ;
 ## OPÇÃO 2:
-#cd "${PATH_REPO_TEST}/${REPO_NAME}" && docker-compose up ;
+#cd "${PATH_REPO_TEST}/${REPO_NAME}" && docker-compose -f "${PATH_REPO_TEST}/${REPO_NAME}/nodejs.compose.yaml"  up ;
 
 
 ### DON'T RUN !!!! ###
